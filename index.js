b265d43ad9e18e4f67cd39ef64fea21100bdea8b
Rida Iqbal <ridariqbal@gmail.com>
2020-06-07 18:16:22 -0400

    Feature - Added randomely appearing moles and updating score

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 let score = 0;
+
+scoreBoard = document.getElementById('score')
+
+const holeList = document.getElementsByClassName('hole')
+
+setInterval ( function () {
+    const assignMole = Math.floor(Math.random() * holeList.length)
+    holeList[assignMole].classList.toggle('mole')
+} ,200)
+
+const game = document.getElementById('whack-a-mole')
+game.addEventListener('click', function(event) {
+    if(event.target.matches('.mole')){
+        score++
+        event.target.classList.remove('mole')
+        scoreBoard.innerText = score
+    }
+})
